04aae2c9b621f5118cf422464f4958a47de1098c
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-08-31 17:45:22 -0300

    dijkstra test ok

--- atividades/roteiro7/grafo_test.py
+++ atividades/roteiro7/grafo_test.py
@@ -27,6 +27,32 @@ class TestGrafo(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0],
         ]
 
+        self.g_p_dijkstra = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_dijkstra.adicionaAresta('a1', 'J', 'C')
+        self.g_p_dijkstra.adicionaAresta('a2', 'C', 'E')
+        self.g_p_dijkstra.adicionaAresta('a3', 'E', 'C')
+        self.g_p_dijkstra.adicionaAresta('a4', 'P', 'C')
+        self.g_p_dijkstra.adicionaAresta('a5', 'C', 'P')
+        self.g_p_dijkstra.adicionaAresta('a6', 'C', 'T')
+        self.g_p_dijkstra.adicionaAresta('a7', 'M', 'C')
+        self.g_p_dijkstra.adicionaAresta('a8', 'M', 'T')
+        self.g_p_dijkstra.adicionaAresta('a9', 'T', 'Z')
+
+        self.g_p_dijkstra_J_Z_gabarito = ['J', 'a1', 'C', 'a6', 'T', 'a9', 'Z']
+
+        self.g_p_dijkstra_com_pesos = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a1', 'J', 'C')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a2', 'C', 'E')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a3', 'E', 'C')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a4', 'P', 'C')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a5', 'C', 'P')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a6', 'C', 'T', 8)
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a7', 'C', 'M')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a8', 'M', 'T')
+        self.g_p_dijkstra_com_pesos.adicionaAresta('a9', 'T', 'Z')
+
+        self.g_p_dijkstra_com_pesos_J_Z_gabarito = ['J', 'a1', 'C', 'a7', 'M', 'a8', 'T', 'a9', 'Z']
+
         # Grafo da Paraíba sem arestas paralelas
         self.g_p_sem_paralelas = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
         self.g_p_sem_paralelas.adicionaAresta('a1', 'J', 'C')
@@ -71,39 +97,6 @@ class TestGrafo(unittest.TestCase):
             [0, 0]
         ]
 
-        self.g_c3 = MeuGrafo(['J'])
-
-        # Grafos com laco
-        self.g_l1 = MeuGrafo(['A', 'B', 'C', 'D'])
-        self.g_l1.adicionaAresta('a1', 'A', 'A')
-        self.g_l1.adicionaAresta('a2', 'A', 'B')
-        self.g_l1.adicionaAresta('a3', 'A', 'A')
-
-        self.g_l2 = MeuGrafo(['A', 'B', 'C', 'D'])
-        self.g_l2.adicionaAresta('a1', 'A', 'B')
-        self.g_l2.adicionaAresta('a2', 'B', 'B')
-        self.g_l2.adicionaAresta('a3', 'B', 'A')
-
-        self.g_l3 = MeuGrafo(['A', 'B', 'C', 'D'])
-        self.g_l3.adicionaAresta('a1', 'C', 'A')
-        self.g_l3.adicionaAresta('a2', 'C', 'C')
-        self.g_l3.adicionaAresta('a3', 'D', 'D')
-        self.g_l3.adicionaAresta('a4', 'D', 'D')
-
-        self.g_l4 = MeuGrafo(['D'])
-        self.g_l4.adicionaAresta('a1', 'D', 'D')
-
-        self.g_l5 = MeuGrafo(['C', 'D'])
-        self.g_l5.adicionaAresta('a1', 'D', 'C')
-        self.g_l5.adicionaAresta('a2', 'C', 'C')
-
-        # Grafos desconexos
-        self.g_d = MeuGrafo(['A', 'B', 'C', 'D'])
-        self.g_d.adicionaAresta('asd', 'A', 'B')
-
-        # Grafo adicionado por João Victor
-        # Grafo com arestas paralelas onde os vértices estão "invertidos"
-        # Exemplo: a1=(A-B) e a2=(B-A)
         self.g_a_p = MeuGrafo(['A', 'B'])
         self.g_a_p.adicionaAresta('a1', 'A', 'B')
         self.g_a_p.adicionaAresta('a2', 'B', 'A')
@@ -128,70 +121,6 @@ class TestGrafo(unittest.TestCase):
         ]
 
 
-        # Grafos gabaritos DFS
-        self.g_p_dfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'T', 'M', 'Z'])
-        self.g_p_dfs_J.adicionaAresta('a1', 'J', 'C')
-        self.g_p_dfs_J.adicionaAresta('a2', 'C', 'E')
-        self.g_p_dfs_J.adicionaAresta('a4', 'P', 'C')
-        self.g_p_dfs_J.adicionaAresta('a6', 'T', 'C')
-        self.g_p_dfs_J.adicionaAresta('a8', 'M', 'T')
-        self.g_p_dfs_J.adicionaAresta('a9', 'T', 'Z')
-
-        self.g_c_dfs_J = MeuGrafo(['J', 'C', 'E', 'P'])
-        self.g_c_dfs_J.adicionaAresta('a1', 'J', 'C')
-        self.g_c_dfs_J.adicionaAresta('a4', 'E', 'C')
-        self.g_c_dfs_J.adicionaAresta('a6', 'P', 'E')
-
-        self.g_a_p_dfs_A = MeuGrafo(['A', 'B'])
-        self.g_a_p_dfs_A.adicionaAresta('a1', 'A', 'B')
-
-        self.g_l1_dfs_A = MeuGrafo(['A', 'B'])
-        self.g_l1_dfs_A.adicionaAresta('a2', 'A', 'B')
-        
-        self.g_c2_dfs_Nina = MeuGrafo(['Nina', 'Maria'])
-        self.g_c2_dfs_Nina.adicionaAresta('amiga', 'Nina', 'Maria')
-
-        
-        self.g_p_sem_paralelas_dfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
-        self.g_p_sem_paralelas_dfs_J.adicionaAresta('a1', 'J', 'C')
-        self.g_p_sem_paralelas_dfs_J.adicionaAresta('a2', 'C', 'E')
-        self.g_p_sem_paralelas_dfs_J.adicionaAresta('a3', 'P', 'C')
-        self.g_p_sem_paralelas_dfs_J.adicionaAresta('a4', 'T', 'C')
-        self.g_p_sem_paralelas_dfs_J.adicionaAresta('a6', 'M', 'T')
-        self.g_p_sem_paralelas_dfs_J.adicionaAresta('a7', 'T', 'Z')
-
-        # Grafos gabaritos BFS
-        self.g_p_bfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'T', 'M', 'Z'])
-        self.g_p_bfs_J.adicionaAresta('a1', 'J', 'C')
-        self.g_p_bfs_J.adicionaAresta('a2', 'C', 'E')
-        self.g_p_bfs_J.adicionaAresta('a4', 'P', 'C')
-        self.g_p_bfs_J.adicionaAresta('a6', 'T', 'C')
-        self.g_p_bfs_J.adicionaAresta('a7', 'M', 'C')
-        self.g_p_bfs_J.adicionaAresta('a9', 'T', 'Z')
-
-        self.g_c_bfs_J = MeuGrafo(['J', 'C', 'E', 'P'])
-        self.g_c_bfs_J.adicionaAresta('a1','J','C')
-        self.g_c_bfs_J.adicionaAresta('a2', 'J', 'E')
-        self.g_c_bfs_J.adicionaAresta('a3', 'J', 'P')
-
-        self.g_l1_bfs_A = MeuGrafo(['A', 'B'])
-        self.g_l1_bfs_A.adicionaAresta('a2', 'A', 'B')
-
-        self.g_a_p_bfs_A = MeuGrafo(['A', 'B'])
-        self.g_a_p_bfs_A.adicionaAresta('a1', 'A', 'B')
-
-        self.g_c2_bfs_Nina = MeuGrafo(['Nina', 'Maria'])
-        self.g_c2_bfs_Nina.adicionaAresta('amiga', 'Nina', 'Maria')
-
-        self.g_p_sem_paralelas_bfs_J = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
-        self.g_p_sem_paralelas_bfs_J.adicionaAresta('a1', 'J', 'C')
-        self.g_p_sem_paralelas_bfs_J.adicionaAresta('a2', 'C', 'E')
-        self.g_p_sem_paralelas_bfs_J.adicionaAresta('a3', 'P', 'C')
-        self.g_p_sem_paralelas_bfs_J.adicionaAresta('a4', 'T', 'C')
-        self.g_p_sem_paralelas_bfs_J.adicionaAresta('a5', 'M', 'C')
-        self.g_p_sem_paralelas_bfs_J.adicionaAresta('a7', 'T', 'Z')
-
-
         # Grafos desconexos
         self.g_desconexo_1 = MeuGrafo(['A', 'B', 'C'])
         self.g_desconexo_1.adicionaAresta('a1', 'A', 'B')
@@ -208,6 +137,37 @@ class TestGrafo(unittest.TestCase):
         self.g_sem_ciclo_2.adicionaAresta('a1', 'D', 'E')
         self.g_sem_ciclo_2.adicionaAresta('a2', 'D', 'F')
 
+        # Grafos testes para Dijkstra
+        self.g_dijkstra1 = MeuGrafo(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+        self.g_dijkstra1.adicionaAresta('a1', 'A', 'B', 1)
+        self.g_dijkstra1.adicionaAresta('a2', 'B', 'C', 1)
+        self.g_dijkstra1.adicionaAresta('a3', 'C', 'D', 1)
+        self.g_dijkstra1.adicionaAresta('a4', 'C', 'E', 2)
+        self.g_dijkstra1.adicionaAresta('a5', 'D', 'E', 3)
+        self.g_dijkstra1.adicionaAresta('a6', 'E', 'G', 2)
+        self.g_dijkstra1.adicionaAresta('a7', 'B', 'F', 3)
+        self.g_dijkstra1.adicionaAresta('a8', 'F', 'G', 1)
+
+        self.g_dijkstra1_A_G_gabarito = ['A', 'a1', 'B', 'a7', 'F', 'a8', 'G']
+
+        self.g_dijkstra2 = MeuGrafo(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        self.g_dijkstra2.adicionaAresta('a1', '1', '2', 2)
+        self.g_dijkstra2.adicionaAresta('a2', '1', '3', 5)
+        self.g_dijkstra2.adicionaAresta('a3', '1', '4', 2)
+        self.g_dijkstra2.adicionaAresta('a4', '2', '3', 3)
+        self.g_dijkstra2.adicionaAresta('a5', '2', '5', 1)
+        self.g_dijkstra2.adicionaAresta('a6', '3', '4', 3)
+        self.g_dijkstra2.adicionaAresta('a7', '5', '3', 1)
+        self.g_dijkstra2.adicionaAresta('a8', '3', '6', 1)
+        self.g_dijkstra2.adicionaAresta('a9', '3', '8', 1)
+        self.g_dijkstra2.adicionaAresta('a10', '4', '7', 2)
+        self.g_dijkstra2.adicionaAresta('a11', '5', '9', 7)
+        self.g_dijkstra2.adicionaAresta('a12', '6', '8', 1)
+        self.g_dijkstra2.adicionaAresta('a13', '7', '6', 2)
+        self.g_dijkstra2.adicionaAresta('a14', '8', '9', 1)
+
+        self.g_dijkstra2_1_9_gabarito = ['1', 'a1', '2', 'a5', '5', 'a7', '3', 'a9', '8', 'a14', '9']
+
 
     def test_warshall(self):
         self.assertListEqual(self.g_p.warshall(), self.g_p_warshall_gabarito)
@@ -216,3 +176,15 @@ class TestGrafo(unittest.TestCase):
         self.assertListEqual(self.g_c.warshall(), self.g_c_warshall_gabarito)
         self.assertListEqual(self.g_c2.warshall(), self.g_c2_warshall_gabarito)
         self.assertListEqual(self.g_a_p.warshall(), self.g_a_p_warshall_gabarito)
+
+        
+    def test_dijkstra(self):
+        self.assertListEqual(self.g_dijkstra1.dijkstra('A', 'G'), self.g_dijkstra1_A_G_gabarito)
+        self.assertListEqual(self.g_dijkstra2.dijkstra('1', '9'), self.g_dijkstra2_1_9_gabarito)
+        self.assertListEqual(self.g_p_dijkstra.dijkstra('J', 'Z'), self.g_p_dijkstra_J_Z_gabarito)
+        self.assertListEqual(self.g_p_dijkstra_com_pesos.dijkstra('J', 'Z'), self.g_p_dijkstra_com_pesos_J_Z_gabarito)
+        self.assertFalse(self.g_desconexo_1.dijkstra('A', 'C'))
+        self.assertFalse(self.g_desconexo_2.dijkstra('A', 'B'))
+        self.assertFalse(self.g_sem_ciclo_1.dijkstra('C', 'A'))
+        self.assertFalse(self.g_sem_ciclo_2.dijkstra('E', 'F'))
+
--- atividades/roteiro7/teste.py
+++ atividades/roteiro7/teste.py
@@ -1,15 +1,16 @@
 from bibgrafo.grafo_matriz_adj_dir import GrafoMatrizAdjacenciaDirecionado
 from meu_grafo import *
 
-g_l1 = MeuGrafo(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
-g_l1.adicionaAresta('a1', 'A', 'B', 1)
-g_l1.adicionaAresta('a2', 'B', 'C', 1)
-g_l1.adicionaAresta('a3', 'C', 'D', 1)
-g_l1.adicionaAresta('a4', 'C', 'E', 2)
-g_l1.adicionaAresta('a5', 'D', 'E', 3)
-g_l1.adicionaAresta('a6', 'E', 'G', 2)
-g_l1.adicionaAresta('a7', 'B', 'F', 3)
-g_l1.adicionaAresta('a8', 'F', 'G', 1)
+g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+g_p.adicionaAresta('a1', 'J', 'C')
+g_p.adicionaAresta('a2', 'C', 'E')
+g_p.adicionaAresta('a3', 'E', 'C')
+g_p.adicionaAresta('a4', 'P', 'C')
+g_p.adicionaAresta('a5', 'C', 'P')
+g_p.adicionaAresta('a6', 'C', 'T')
+g_p.adicionaAresta('a7', 'M', 'C')
+g_p.adicionaAresta('a8', 'M', 'T')
+g_p.adicionaAresta('a9', 'T', 'Z')
 
-print(g_l1.dijkstra('A', 'G'))
+print(g_p.dijkstra('J', 'Z'))
 
