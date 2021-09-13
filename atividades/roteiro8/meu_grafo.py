db9970549b0c8f42e0eb4b927868643971caec4a
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-09-13 20:13:47 -0300

    prim ok

---
+++ atividades/roteiro8/meu_grafo.py
@@ -0,0 +1,372 @@
+from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
+from bibgrafo.grafo_exceptions import *
+from copy import deepcopy
+from time import sleep
+
+class MeuGrafo(GrafoListaAdjacencia):
+
+    def vertices_nao_adjacentes(self):
+        '''
+        Provê uma lista de vértices não adjacentes no grafo. A lista terá o seguinte formato: [X-Z, X-W, ...]
+        Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+        :return: Uma lista com os pares de vértices não adjacentes
+        '''
+        vertices_nao_adjacentes = []
+
+        for v in self.N:
+            vertices_adjacentes = []
+            for a in self.A:
+                v1 = self.A[a].getV1()
+                v2 = self.A[a].getV2()
+                if v1 == v:
+                    vertices_adjacentes.append(v2)
+                elif v2 == v:
+                    vertices_adjacentes.append(v1)
+
+            for vt in self.N:
+                if vt != v and vt not in vertices_adjacentes:
+                    a_test_1 = f'{v}-{vt}'
+                    a_test_2 = f'{vt}-{v}'
+                    if a_test_1 not in vertices_nao_adjacentes and a_test_2 not in vertices_nao_adjacentes:
+                        vertices_nao_adjacentes.append(f'{v}-{vt}')
+
+            vertices_adjacentes = []
+
+        return vertices_nao_adjacentes
+
+    def ha_laco(self):
+        '''
+        Verifica se existe algum laço no grafo.
+        :return: Um valor booleano que indica se existe algum laço.
+        '''
+        for a in self.A:
+            if self.A[a].getV1() == self.A[a].getV2():
+                return True
+
+        return False
+
+
+    def grau(self, V=''):
+        '''
+        Provê o grau do vértice passado como parâmetro
+        :param V: O rótulo do vértice a ser analisado
+        :return: Um valor inteiro que indica o grau do vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+
+
+        vertices_adjacentes = []
+
+        verticeExiste = False
+
+        for vt in self.N:
+            if vt == V:
+                verticeExiste = True
+                vertices_adjacentes = []
+                for a in self.A:
+                    v1 = self.A[a].getV1()
+                    v2 = self.A[a].getV2()
+                    if v1 == vt:
+                        vertices_adjacentes.append(v2)
+                    if v2 == vt:
+                        vertices_adjacentes.append(v1)
+
+        if len(vertices_adjacentes) == 0 and verticeExiste == False:
+            raise VerticeInvalidoException('O vértice ' + V + ' não existe no grafo')
+        else:
+            return len(vertices_adjacentes)
+
+
+
+    def ha_paralelas(self):
+        '''
+        Verifica se há arestas paralelas no grafo
+        :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+        '''
+        
+        for a1 in self.A:
+            v1_a1 = self.A[a1].getV1()
+            v2_a1 = self.A[a1].getV2()
+            
+            a1_str = f'{v1_a1}-{v2_a1}'
+            a1_str_swap = f'{v2_a1}-{v1_a1}'
+
+            for a2 in self.A:
+                if a1 != a2:
+                    v1_a2 = self.A[a2].getV1()
+                    v2_a2 = self.A[a2].getV2()
+                    a2_str = f'{v1_a2}-{v2_a2}'
+                    a2_str_swap = f'{v2_a2}-{v1_a2}'
+
+                    if a1_str == a2_str or a1_str == a2_str_swap or a2_str == a1_str_swap:
+                        return True
+
+        return False
+
+    def arestas_sobre_vertice(self, V):
+        '''
+        Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+        :param V: O vértice a ser analisado
+        :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+        arestas_adjacentes = []
+
+        verticeExiste = False
+
+        for vt in self.N:
+            if vt == V:
+                verticeExiste = True
+                for a in self.A:
+                    v1 = self.A[a].getV1()
+                    v2 = self.A[a].getV2()
+                    if v1 == vt or v2 == vt:
+                        if a not in arestas_adjacentes:
+                            arestas_adjacentes.append(a)
+
+        if len(arestas_adjacentes) == 0 and verticeExiste == False:
+            raise VerticeInvalidoException('O vértice ' + V + ' não existe no grafo')
+        else:
+            return arestas_adjacentes
+
+
+    def eh_completo(self):
+        '''
+        Verifica se o grafo é completo.
+        :return: Um valor booleano que indica se o grafo é completo
+        '''
+        if self.ha_laco() or self.ha_paralelas():
+            return False
+
+        else:
+            totalVertices = len(self.N)
+            for v in self.N:
+                if self.grau((v)) == totalVertices-1:
+                    continue
+                else:
+                    return False
+
+            return True
+
+
+    
+
+    def caminho_dois_vertices(self, x, y):
+        '''
+        Realiza uma verificação se existe um caminho entre os dois vértices passados nos parâmetros
+        :return: Uma lista contendo um valor booleano que representa se existe caminho ou não,
+        e o tamanho do caminho (caso exista)
+
+        Exemplo:
+        grafo.caminho_dois_vertices('x', 'y')
+        :return: [true, 4]
+        '''
+        finalizada = False
+
+        vertices_examinados = {}
+
+        for vt in self.N:
+            vertices_examinados[vt] = {
+                'examinado': False,
+                'root': False,
+                'pai': False,
+                'temPai': False,
+                'arestaPai': ''
+            }
+
+        vertices_examinados[x] = {
+            'examinado': True,
+            'root': True,
+            'pai': x,
+            'temPai': True,
+            'arestaPai': ''
+        }
+        
+
+        vertice_atual = x
+        vertice_final = y
+        comeco = True   
+
+        existeCaminho = False
+        tamanhoCaminho = 0
+        fila_vertices = []
+
+        while (finalizada == False):
+            arestas_incidentes = self.arestas_sobre_vertice(vertice_atual)
+            if len(arestas_incidentes) == 0:
+                finalizada = True
+                break
+            
+            for a in arestas_incidentes:
+                v1 = self.A[a].getV1()
+                v2 = self.A[a].getV2()
+                if v1 == vertice_atual:
+                    if not vertices_examinados[v2]['temPai']:
+                        vertices_examinados[v2]['pai'] = v1
+                        vertices_examinados[v2]['temPai'] = True
+                        vertices_examinados[v2]['arestaPai'] = a
+
+                    if vertices_examinados[v2]['examinado'] == False and v2 not in fila_vertices:
+                        fila_vertices.append(v2)
+
+                if v2 == vertice_atual:
+                    if not vertices_examinados[v1]['temPai']:
+                        vertices_examinados[v1]['pai'] = v2
+                        vertices_examinados[v1]['temPai'] = True
+                        vertices_examinados[v1]['arestaPai'] = a
+
+                    if vertices_examinados[v1]['examinado'] == False and v1 not in fila_vertices:
+                        fila_vertices.append(v1)
+
+                if vertice_atual == y:
+                    finalizada = True
+                    existeCaminho = True
+                    break
+
+            vertices_examinados[vertice_atual]['examinado'] = True
+
+            
+
+            if vertices_examinados[vertice_atual]['root'] == False:
+                v_pai = vertices_examinados[vertice_atual]['pai']
+                aresta_do_pai = vertices_examinados[vertice_atual]['arestaPai']
+
+
+
+            if vertice_atual in fila_vertices:
+               fila_vertices.remove(vertice_atual)
+               if len(fila_vertices) == 0:
+                   finalizada = True
+                   break
+            
+            
+            vertice_atual = fila_vertices[0]
+
+        
+        lista_final = []
+        if (existeCaminho):
+            lista_preenchida = False
+            lista_aux = []
+            v_atual = vertice_final
+            while(not lista_preenchida):
+                if v_atual == x:
+                    lista_preenchida = True
+                    break
+                lista_aux.append(vertices_examinados[v_atual]['arestaPai'])
+                v_atual = vertices_examinados[v_atual]['pai']
+
+            for i in range(len(lista_aux)-1, -1, -1):
+                lista_final.append(lista_aux[i])
+
+
+        return [existeCaminho, len(lista_final)]
+
+
+
+    def conexo(self):
+        '''
+        Verifica se um grafo é conexo ou não
+        :return: Valor booleano que representa se o grafo é conexo ou não
+        '''
+        lista_vertices = deepcopy(self.N)
+        vertice_escolhido = lista_vertices[0]
+        resto_da_lista = lista_vertices[1:]
+
+        ehConexo = True
+
+        for v in resto_da_lista:
+            if not (self.caminho_dois_vertices(vertice_escolhido, v)[0]):
+                ehConexo = False
+        
+        return ehConexo
+
+
+    def vertice_menor_peso(self):
+        grafo_copia = deepcopy(self)
+        arestas = list(grafo_copia.A.keys())
+
+        aresta_menor_peso = arestas[0]
+        for i in arestas:
+            if (grafo_copia.A[i].getPeso() < grafo_copia.A[aresta_menor_peso].getPeso()):
+                aresta_menor_peso = i
+
+        return [grafo_copia.A[aresta_menor_peso].getV1(), grafo_copia.A[aresta_menor_peso].getV2(), aresta_menor_peso]
+
+
+    def aresta_menor_peso(self, dict_arestas):
+        arestas = list(dict_arestas.keys())
+
+        aresta_menor_peso = arestas[0]
+        for i in arestas:
+            if (dict_arestas[i].getPeso() < dict_arestas[aresta_menor_peso].getPeso()):
+                aresta_menor_peso = i
+
+        return aresta_menor_peso
+
+    
+    def prim(self):
+        grafo_copia = deepcopy(self)
+        arestas = list(grafo_copia.A.keys())
+        vertices = deepcopy(grafo_copia.N)
+
+
+        v1, v2, a = grafo_copia.vertice_menor_peso()
+        
+        # pis = {}
+        # for v in vertices:
+        #     pis[v] = NULL
+
+        grafo_prim = MeuGrafo([v1, v2])
+        grafo_prim.adicionaAresta(a, v1, v2)
+        vertices_analisados = []
+
+        fim_algoritmo = False
+
+        while(not fim_algoritmo):
+            vertices_na_arvore = grafo_prim.N
+            vertices_nao_estao_na_arvore = []
+
+            for v in vertices:
+                if v not in vertices_na_arvore:
+                    vertices_nao_estao_na_arvore.append(v)
+
+            if len(vertices_nao_estao_na_arvore) == 0:
+                fim_algoritmo = True
+                break
+
+
+            arestas_adjacentes_a_arvore = []
+            for v in vertices_na_arvore:
+                arestas_sobre_v = grafo_copia.arestas_sobre_vertice(v)
+                for a in arestas_sobre_v:
+                    if a not in arestas_adjacentes_a_arvore:
+                        arestas_adjacentes_a_arvore.append(a)
+
+
+
+            arestas_fora_da_arvore = {}
+
+            for aresta in list(grafo_copia.A.keys()):
+                if aresta not in list(grafo_prim.A.keys()):
+                    if aresta in arestas_adjacentes_a_arvore:
+                        arestas_fora_da_arvore[aresta] = deepcopy(grafo_copia.A[aresta])
+            
+            aresta_menor_peso = grafo_copia.aresta_menor_peso(arestas_fora_da_arvore)
+
+            v1 = grafo_copia.A[aresta_menor_peso].getV1()
+            v2 = grafo_copia.A[aresta_menor_peso].getV2()
+
+            if v1 not in grafo_prim.N:
+                grafo_prim.adicionaVertice(v1)
+                grafo_prim.adicionaAresta(aresta_menor_peso, v2, v1, grafo_copia.A[aresta_menor_peso].getPeso())
+            else:
+                grafo_prim.adicionaVertice(v2)
+                grafo_prim.adicionaAresta(aresta_menor_peso, v1, v2, grafo_copia.A[aresta_menor_peso].getPeso())
+
+
+        return grafo_prim
+                
+
+                
+        
+
---
+++ atividades/roteiro8/teste.py
@@ -0,0 +1,15 @@
+from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
+from meu_grafo import *
+
+grafo = MeuGrafo(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+grafo.adicionaAresta('a1', 'A', 'B', 1)
+grafo.adicionaAresta('a2', 'B', 'C', 2)
+grafo.adicionaAresta('a3', 'C', 'G', 2)
+grafo.adicionaAresta('a4', 'G', 'F', 3)
+grafo.adicionaAresta('a5', 'E', 'F', 4)
+grafo.adicionaAresta('a6', 'E', 'G', 1)
+grafo.adicionaAresta('a7', 'E', 'D', 2)
+grafo.adicionaAresta('a8', 'A', 'D', 3)
+grafo.adicionaAresta('a9', 'B', 'G', 5)
+grafo.adicionaAresta('a10', 'D', 'G', 3)
+print(grafo.prim())
