2c00f957bcae6ee56606914e47e3eede105611ac
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-09-21 18:35:05 -0300

    roteiro 9 tests and code ok

--- atividades/roteiro9/grafo_test.py
+++ atividades/roteiro9/grafo_test.py
@@ -333,9 +333,82 @@ class TestGrafo(unittest.TestCase):
             '61', '62', '63', '64', '86', '71', '84']
 
 
+        # Grafo do curso de Licenciatura em Física
+        self.lic_letras = MeuGrafo(
+            [
+                '11', '12', '13', '14', '15', '16', '17',
+                '21', '22', '23', '24', '25', '26', '27',
+                '31', '32', '33', '34', '35', '36', '37',
+                '41', '42', '43', '44', '45', '46', '47',
+                '51', '52', '53', '54', '55', '56', '57',
+                '61', '62', '63', '64', '65', '66', '67', '68',
+                '71', '72', '73', '74', '75', '76', '77', '78',
+                '81', '82', '83', '84', '85', '86', '87', '88'
+            ]
+        )
+
+        self.lic_letras.adicionaAresta('d1', '11', '21')
+        self.lic_letras.adicionaAresta('d3', '11', '22')
+        self.lic_letras.adicionaAresta('d5', '12', '23')
+        self.lic_letras.adicionaAresta('d8', '12', '25')
+        self.lic_letras.adicionaAresta('d8', '17', '26')
+        self.lic_letras.adicionaAresta('d9', '21', '31')
+        self.lic_letras.adicionaAresta('d10', '21', '32')
+        self.lic_letras.adicionaAresta('d10', '21', '33')
+        self.lic_letras.adicionaAresta('d11', '24', '34')
+        self.lic_letras.adicionaAresta('d12', '25', '35')
+        self.lic_letras.adicionaAresta('d13', '31', '41')
+        self.lic_letras.adicionaAresta('d14', '33', '42')
+        self.lic_letras.adicionaAresta('d15', '25', '43')
+        self.lic_letras.adicionaAresta('d16', '25', '44')
+        self.lic_letras.adicionaAresta('d17', '36', '44')
+        self.lic_letras.adicionaAresta('d171', '23', '46')
+        self.lic_letras.adicionaAresta('d172', '35', '46')
+        self.lic_letras.adicionaAresta('d173', '37', '47')
+        self.lic_letras.adicionaAresta('d18', '31', '51')
+        self.lic_letras.adicionaAresta('d19', '35', '52')
+        self.lic_letras.adicionaAresta('d20', '13', '53')
+        self.lic_letras.adicionaAresta('d21', '45', '54')
+        self.lic_letras.adicionaAresta('d22', '35', '55')
+        self.lic_letras.adicionaAresta('d23', '22', '56')
+        self.lic_letras.adicionaAresta('d26', '43', '57')
+        self.lic_letras.adicionaAresta('d27', '31', '61')
+        self.lic_letras.adicionaAresta('d28', '31', '62')
+        self.lic_letras.adicionaAresta('d30', '35', '63')
+        self.lic_letras.adicionaAresta('d32', '54', '64')
+        self.lic_letras.adicionaAresta('d34', '37', '67')
+        self.lic_letras.adicionaAresta('d35', '54', '68')
+        self.lic_letras.adicionaAresta('d36', '31', '71')
+        self.lic_letras.adicionaAresta('d37', '31', '72')
+        self.lic_letras.adicionaAresta('d39', '31', '73')
+        self.lic_letras.adicionaAresta('d40', '64', '74')
+        self.lic_letras.adicionaAresta('d41', '35', '75')
+        self.lic_letras.adicionaAresta('d51', '45', '76')
+        self.lic_letras.adicionaAresta('d52', '27', '77')
+        self.lic_letras.adicionaAresta('d53', '53', '77')
+        self.lic_letras.adicionaAresta('d54', '64', '78')
+        self.lic_letras.adicionaAresta('d55', '68', '78')
+        self.lic_letras.adicionaAresta('d44', '17', '83')
+        self.lic_letras.adicionaAresta('d45', '74', '84')
+        self.lic_letras.adicionaAresta('d48', '77', '87')
+        self.lic_letras.adicionaAresta('d49', '74', '88')
+        self.lic_letras.adicionaAresta('d50', '78', '88')
+
+        self.lic_letras_gabarito = [
+            '11', '12', '13', '14', '15', '16', '17',
+            '24', '27', '36', '37', '45', '65', '66',
+            '81', '82', '85', '86', '21', '22', '23',
+            '25', '26', '34', '47', '53', '54', '67',
+            '76', '83', '31', '32', '33', '35', '43',
+            '44', '56', '64', '68', '77', '41', '42',
+            '46', '51', '52', '55', '57', '61', '62',
+            '63', '71', '72', '73', '74', '75', '78',
+            '87', '84', '88']
+
     def test_kahn(self):
         self.assertListEqual(self.lic_matematica.kahn(), self.lic_matematica_gabarito)
         self.assertListEqual(self.lic_fisica.kahn(), self.lic_fisica_gabarito)
         self.assertListEqual(self.telematica.kahn(), self.telematica_gabarito)
         self.assertListEqual(self.construcao_edif.kahn(), self.construcao_edif_gabarito)
         self.assertListEqual(self.eng_comp.kahn(), self.eng_comp_gabarito)
+        self.assertListEqual(self.lic_letras.kahn(), self.lic_letras_gabarito)
--- atividades/roteiro9/teste.py
+++ atividades/roteiro9/teste.py
@@ -1,80 +1,72 @@
 from bibgrafo.grafo_matriz_adj_dir import GrafoMatrizAdjacenciaDirecionado
 from meu_grafo import *
 
-lic_fisica = MeuGrafo(
+lic_letras = MeuGrafo(
   [
     '11', '12', '13', '14', '15', '16', '17',
     '21', '22', '23', '24', '25', '26', '27',
     '31', '32', '33', '34', '35', '36', '37',
-    '41', '42', '43', '44', '45', '46',
+    '41', '42', '43', '44', '45', '46', '47',
     '51', '52', '53', '54', '55', '56', '57',
-    '61', '62', '63', '64', '65', '66', '68',
-    '71', '72', '73', '74', '76',
-    '81', '82', '83', '84', '85', '86'
+    '61', '62', '63', '64', '65', '66', '67', '68',
+    '71', '72', '73', '74', '75', '76', '77', '78',
+    '81', '82', '83', '84', '85', '86', '87', '88'
   ]
 )
 
-lic_fisica.adicionaAresta('d1', '11', '21')
-lic_fisica.adicionaAresta('d2', '12', '21')
-lic_fisica.adicionaAresta('d3', '11', '22')
-lic_fisica.adicionaAresta('d4', '12', '22')
-lic_fisica.adicionaAresta('d5', '12', '23')
-lic_fisica.adicionaAresta('d6', '12', '24')
-lic_fisica.adicionaAresta('d7', '14', '24')
-lic_fisica.adicionaAresta('d8', '15', '25')
+lic_letras.adicionaAresta('d1', '11', '21')
+lic_letras.adicionaAresta('d3', '11', '22')
+lic_letras.adicionaAresta('d5', '12', '23')
+lic_letras.adicionaAresta('d8', '12', '25')
+lic_letras.adicionaAresta('d8', '17', '26')
 
-lic_fisica.adicionaAresta('d9', '21', '31')
-lic_fisica.adicionaAresta('d10', '23', '31')
-lic_fisica.adicionaAresta('d10', '21', '31')
-lic_fisica.adicionaAresta('d11', '22', '32')
-lic_fisica.adicionaAresta('d12', '23', '33')
+lic_letras.adicionaAresta('d9', '21', '31')
+lic_letras.adicionaAresta('d10', '21', '32')
+lic_letras.adicionaAresta('d10', '21', '33')
+lic_letras.adicionaAresta('d11', '24', '34')
+lic_letras.adicionaAresta('d12', '25', '35')
 
-lic_fisica.adicionaAresta('d13', '31', '41')
-lic_fisica.adicionaAresta('d14', '31', '42')
-lic_fisica.adicionaAresta('d15', '32', '42')
-lic_fisica.adicionaAresta('d16', '33', '45')
-lic_fisica.adicionaAresta('d17', '31', '46')
+lic_letras.adicionaAresta('d13', '31', '41')
+lic_letras.adicionaAresta('d14', '33', '42')
+lic_letras.adicionaAresta('d15', '25', '43')
+lic_letras.adicionaAresta('d16', '25', '44')
+lic_letras.adicionaAresta('d17', '36', '44')
+lic_letras.adicionaAresta('d171', '23', '46')
+lic_letras.adicionaAresta('d172', '35', '46')
+lic_letras.adicionaAresta('d173', '37', '47')
 
-lic_fisica.adicionaAresta('d18', '41', '51')
-lic_fisica.adicionaAresta('d19', '45', '51')
-lic_fisica.adicionaAresta('d20', '41', '52')
-lic_fisica.adicionaAresta('d21', '42', '52')
-lic_fisica.adicionaAresta('d22', '45', '53')
-lic_fisica.adicionaAresta('d23', '31', '54')
-lic_fisica.adicionaAresta('d24', '43', '55')
-lic_fisica.adicionaAresta('d25', '21', '57')
-lic_fisica.adicionaAresta('d26', '43', '57')
+lic_letras.adicionaAresta('d18', '31', '51')
+lic_letras.adicionaAresta('d19', '35', '52')
+lic_letras.adicionaAresta('d20', '13', '53')
+lic_letras.adicionaAresta('d21', '45', '54')
+lic_letras.adicionaAresta('d22', '35', '55')
+lic_letras.adicionaAresta('d23', '22', '56')
+lic_letras.adicionaAresta('d26', '43', '57')
 
-lic_fisica.adicionaAresta('d27', '51', '61')
-lic_fisica.adicionaAresta('d28', '51', '62')
-lic_fisica.adicionaAresta('d29', '52', '62')
-lic_fisica.adicionaAresta('d30', '21', '63')
-lic_fisica.adicionaAresta('d31', '53', '63')
-lic_fisica.adicionaAresta('d32', '51', '64')
-lic_fisica.adicionaAresta('d33', '56', '66')
-lic_fisica.adicionaAresta('d34', '31', '68')
-lic_fisica.adicionaAresta('d35', '57', '68')
+lic_letras.adicionaAresta('d27', '31', '61')
+lic_letras.adicionaAresta('d28', '31', '62')
+lic_letras.adicionaAresta('d30', '35', '63')
+lic_letras.adicionaAresta('d32', '54', '64')
+lic_letras.adicionaAresta('d34', '37', '67')
+lic_letras.adicionaAresta('d35', '54', '68')
 
-lic_fisica.adicionaAresta('d36', '61', '71')
-lic_fisica.adicionaAresta('d37', '41', '72')
-lic_fisica.adicionaAresta('d38', '45', '72')
-lic_fisica.adicionaAresta('d39', '66', '73')
-lic_fisica.adicionaAresta('d40', '31', '74')
-lic_fisica.adicionaAresta('d41', '43', '74')
-lic_fisica.adicionaAresta('d51', '41', '76')
-lic_fisica.adicionaAresta('d52', '68', '76')
+lic_letras.adicionaAresta('d36', '31', '71')
+lic_letras.adicionaAresta('d37', '31', '72')
+lic_letras.adicionaAresta('d39', '31', '73')
+lic_letras.adicionaAresta('d40', '64', '74')
+lic_letras.adicionaAresta('d41', '35', '75')
+lic_letras.adicionaAresta('d51', '45', '76')
+lic_letras.adicionaAresta('d52', '27', '77')
+lic_letras.adicionaAresta('d53', '53', '77')
+lic_letras.adicionaAresta('d54', '64', '78')
+lic_letras.adicionaAresta('d55', '68', '78')
 
-lic_fisica.adicionaAresta('d42', '65', '81')
-lic_fisica.adicionaAresta('d43', '74', '82')
-lic_fisica.adicionaAresta('d44', '73', '83')
-lic_fisica.adicionaAresta('d45', '54', '84')
-lic_fisica.adicionaAresta('d46', '71', '84')
-lic_fisica.adicionaAresta('d47', '16', '85')
-lic_fisica.adicionaAresta('d48', '25', '85')
-lic_fisica.adicionaAresta('d49', '51', '86')
-lic_fisica.adicionaAresta('d50', '76', '86')
+lic_letras.adicionaAresta('d44', '17', '83')
+lic_letras.adicionaAresta('d45', '74', '84')
+lic_letras.adicionaAresta('d48', '77', '87')
+lic_letras.adicionaAresta('d49', '74', '88')
+lic_letras.adicionaAresta('d50', '78', '88')
 
 
-
-print(lic_fisica.kahn())
+print(lic_letras.kahn())
 
